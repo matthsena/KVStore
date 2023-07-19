2c38b7c02b31d62fea5deef384491f6df5ca17b7
matheus <matheusalexandre.sena99@gmail.com>
2023-07-19 20:00:14 -0300

    feat: esboço TCP

--- src/Client.java
+++ src/Client.java
@@ -3,21 +3,97 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Client {
   public static void main(String[] args) throws IOException {
-    Socket socket = new Socket("localhost", 1234);
+    List<String> servers = new ArrayList<>();
 
-    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    try (Scanner scanner = new Scanner(System.in)) {
+      for (int i = 0; i < 3; i++) {
+        System.out.print("Enter server host and port (e.g. localhost:1234): ");
+        String input = scanner.nextLine();
+        servers.add(input);
+      }
 
-    out.println("Hello, server!");
+      Random random = new Random();
+      int index = random.nextInt(servers.size());
+      String server = servers.get(index);
 
-    String response = in.readLine();
-    System.out.println("Received response from server: " + response);
+      System.out.println("Connecting to server " + server);
+      String[] parts = server.split(":");
+      String host = parts[0];
+      int port = Integer.parseInt(parts[1]);
 
-    in.close();
-    out.close();
-    socket.close();
+      Socket socket = new Socket(host, port);
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+      int[] timestamp = { 0 };
+
+      Thread thread = new Thread(() -> {
+        while (true) {
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          timestamp[0]++;
+        }
+      });
+      thread.start();
+
+      while (true) {
+        System.out.println("Select an option:");
+        System.out.println("1. PUT");
+        System.out.println("2. GET");
+        System.out.println("3. Exit");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        if (choice == 1) {
+          System.out.print("Enter key-value item (e.g. key=value): ");
+          String input = scanner.nextLine();
+
+          out.println("PUT " + timestamp[0] + " " + input);
+
+          String response = in.readLine();
+          if (response.equals("PUT_OK")) {
+            System.out.println("PUT request successful");
+          } else {
+            System.out.println("PUT request failed:" + response);
+          }
+        } else if (choice == 2) {
+          System.out.print("Enter key: ");
+          String key = scanner.nextLine();
+
+          out.println("GET " + key);
+
+          String response = in.readLine();
+          if (response.startsWith("GET_OK")) {
+            String[] parts2 = response.split(" ");
+            int timestamp2 = Integer.parseInt(parts2[1]);
+            String value = parts2[2];
+            System.out.println("Value for key " + key + " (timestamp " + timestamp2 + "): " + value);
+          } else {
+            System.out.println("GET request failed: " + response);
+          }
+        } else if (choice == 3) {
+          break;
+        } else {
+          System.out.println("Invalid choice");
+        }
+      }
+
+      thread.interrupt();
+      in.close();
+      out.close();
+      socket.close();
+    }
   }
 }
--- src/Server.java
+++ src/Server.java
@@ -4,20 +4,46 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+  private static ConcurrentHashMap<String, String> keyValueStore = new ConcurrentHashMap<>();
+
   public static void main(String[] args) throws IOException {
-    try (ServerSocket serverSocket = new ServerSocket(1234)) {
+    String host;
+    int port;
+    int leaderPort;
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.print("Enter the host name: ");
+      host = scanner.nextLine();
+
+      System.out.print("Enter the port number: ");
+      port = scanner.nextInt();
+
+      System.out.print("Enter the leader port number: ");
+      leaderPort = scanner.nextInt();
+    }
+
+    String leaderHost = host;
+
+    System.out.println("Starting server on " + host + ":" + port);
+    System.out.println("Leader is " + leaderHost + ":" + leaderPort);
+
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
       while (true) {
         Socket clientSocket = serverSocket.accept();
-        Thread thread = new Thread(() -> handleClientRequest(clientSocket));
-        thread.start();
+        new Thread(() -> handleClientRequest(clientSocket, host, port, leaderHost, leaderPort)).start();
       }
+    } catch (IOException e) {
+      System.err.println("Could not listen on port " + port);
+      System.exit(-1);
     }
   }
 
-  private static void handleClientRequest(Socket clientSocket) {
+  private static void handleClientRequest(Socket clientSocket, String host, int port, String leaderHost,
+      int leaderPort) {
     try {
       BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
       PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
@@ -25,21 +51,84 @@ public class Server {
       String inputLine;
       while ((inputLine = in.readLine()) != null) {
         System.out.println("Received message from client: " + inputLine);
-        out.println("Server received message: " + inputLine);
 
-        // Add a random delay between 0 and 10 seconds
-        Random random = new Random();
-        int delay = random.nextInt(10000);
-        Thread.sleep(delay);
+        // Parse the HTTP request method
+        String[] parts = inputLine.split(" ");
+        String method = parts[0];
 
-        System.out.println("End Delay: " + delay);
+        // Check if this server is the leader
+        if (host.equals(leaderHost) && port == leaderPort && method.equals("PUT")) {
+          System.out.println("Receive PUT");
+          // This server is the leader and the method is PUT, handle the request
+          parts = inputLine.split(" ");
+          String key = parts[1];
+          String value = parts[2];
+          int[] servers = { 10098, 10099 };
+
+          keyValueStore.put(key, value);
+
+          // Replicate the value to the other servers
+          replicateValue(key, value, servers);
+
+          out.println("PUT_OK");
+        } else if (method.equals("PUT")) {
+          System.out.println("redirect to leader...");
+          // This server is not the leader and the method is PUT, redirect the request to
+          // the leader
+          try (Socket socket = new Socket(leaderHost, leaderPort);
+              PrintWriter outToLeader = new PrintWriter(socket.getOutputStream(), true);
+              BufferedReader inFromLeader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            outToLeader.println(inputLine);
+            String response = inFromLeader.readLine();
+            out.println(response);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        } else if (method.equals("GET")) {
+          // This server received a GET request, return the corresponding value
+          parts = inputLine.split(" ");
+          String key = parts[1];
+
+          String value = keyValueStore.get(key);
+          if (value != null) {
+            out.println(value);
+          } else {
+            out.println("Key not found");
+          }
+        } else if (method.equals("REPLICATION")) {
+          // This server received a replication request, update the local
+          // ConcurrentHashMap
+          parts = inputLine.split(" ");
+          String key = parts[1];
+          String value = parts[2];
+
+          keyValueStore.put(key, value);
+        } else {
+          // This server is not the leader or the method is not PUT, GET, or REPLICATION,
+          // process the request
+          out.println("Server received message: " + inputLine);
+        }
       }
 
       in.close();
       out.close();
       clientSocket.close();
-    } catch (IOException | InterruptedException e) {
+    } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  private static void replicateValue(String key, String value, int[] ports) {
+    // Replicate the value to the other servers using threads
+    for (int port : ports) {
+      new Thread(() -> {
+        try (Socket socket = new Socket("localhost", port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+          out.println("REPLICATION " + key + " " + value);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }).start();
     }
   }
 }
